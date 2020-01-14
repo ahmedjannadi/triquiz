9cea4e54625678e34997960bd4d656418c86d5fe
Ahmed Jannadi <jannadiahmed@gmail.com>
2020-01-14 15:33:30 +0100

    added basic question fetching from backend

--- game/load.js
+++ game/load.js
@@ -1,17 +1,18 @@
 Game.Load = function(game){}
 
 Game.Load.prototype = {
-	preload: async function(){
-		this.questions = await fetch("http://ahmedjannadi.tk:8080/api/questions")
-		console.log(this.questions)
+	preload: function(){
 	},
 	update: function(){
 
 	},
-	create: function(){
-		game.state.start("Play")
+	create: async function(){
 		//game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		//game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
 		game.scale.setScreenSize();
+		let res = await fetch("http://ahmedjannadi.tk:8080/api/questions")
+		let data = await res.json()
+		Game.questions = await JSON.parse(data)
+		game.state.start("Play")
 	},
 }
--- game/play.js
+++ game/play.js
@@ -1,12 +1,18 @@
 Game.Play = function(game){}
-var a
 Game.Play.prototype = {
 	create: function() {
-		
-		this.test = game.add.text(100,100,"testing")
-		this.test.addColor("#fff",0);
+		this.questionCounter=0
+		this.spawnQuestion()
+		let timer = game.time.create(false)
+		timer.loop(10000,this.spawnQuestion, this)
+		timer.start()
 	},
 	update: function() {
 		this.test.y += 2
 	},
+
+	spawnQuestion: function() {
+		this.test = game.add.text(100,100,Game.questions[this.questionCounter++].fields.title,{font: "bold 20px Verdana"})
+		this.test.addColor("#fff",0);
+	}
 }
