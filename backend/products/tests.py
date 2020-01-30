c24e79a6b116706bfeb96e5ddffe82db96930fbd
Ahmed Jannadi <jannadiahmed@gmail.com>
2020-01-30 23:06:18 +0100

    added random background color

--- backend/products/tests.py
+++ backend/products/tests.py
@@ -1,3 +1,15 @@
+import json
 from django.test import TestCase
+from django.http import request
+from products import views
 
-# Create your tests here.
+def is_json(data):
+	try:
+		json.loads(data)
+	except ValueError as e:
+		return False
+	return True
+
+class JsonTestCase(TestCase):
+	def test_json(self):
+		self.assertTrue(views.question(request))
